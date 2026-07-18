3ee98e3ef8a3daf2824dbb3bab2926fb3dd5eb89
Devadharshini S <niladeva631@gmail.com>
2026-07-18 22:05:57 +0530

    Create Data types program.java

---
+++ Data types program.java
@@ -0,0 +1,23 @@
+class Main {
+    public static void main(String[] args) {
+        {
+            int a=5;
+            int b=1;
+            String str="nila";
+            String str1="selvaraj";
+            String str2="sundariyammal";
+            float f1=3.14f;
+            float f2=3.15f;
+            double d=7.77777777d;
+            char c='w';
+            System.out.println("name=" + str);
+            System.out.println("father name=" + str1);
+            System.out.println("mother name="+str2);
+            System.out.println("digits="+a);
+            System.out.println("digits 1="+b);
+            System.out.println("values="+d);
+            System.out.println("words="+c);
+        } 
+        
+    }
+}
