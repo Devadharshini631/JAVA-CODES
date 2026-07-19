245ee7d0f88915914b98785e3ee98b88904bafe4
Devadharshini S <niladeva631@gmail.com>
2026-07-19 20:44:05 +0530

    Create Arithmetic Operator.java

---
+++ Arithmetic Operator.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 20000;
+        int b = 80000;
+        
+        System.out.println("First Number = " + a);
+        System.out.println("Second Number="+b);
+        
+        System.out.println("Addition="+(a+b));
+        System.out.println("Subraction="+(a-b));
+        System.out.println("Multiplication="+(a*b));
+        System.out.println("Divide="+(a/b));
+        System.out.println("Modulus="+(a%b));
+    }
+}
+        
+        
