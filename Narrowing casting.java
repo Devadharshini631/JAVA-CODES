ea6482a5c77ef4995f61d00f3fe32b41cc6fb1be
Devadharshini S <niladeva631@gmail.com>
2026-07-19 14:59:41 +0530

    Create Narrowing casting.java

---
+++ Narrowing casting.java
@@ -0,0 +1,21 @@
+class Main {
+    public static void main(String[] args) {
+        double salary=499.000;
+        double marks=66.888;
+        double price=88.777;
+        
+        int newsalary=(int) salary ;
+        int newmarks=(int) marks;
+        int newprice=(int) price ;
+        
+        System.out.println("original salary="+salary);
+        System.out.println("new salary="+newsalary);
+         
+        System.out.println("original marks="+marks);
+        System.out.println("new marks="+newmarks);
+         
+        System.out.println("original price="+price);
+        System.out.println("new price="+newprice);
+        
+    }
+}
