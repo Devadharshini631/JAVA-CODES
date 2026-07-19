09fa4ef0a8203c253bf2fe8d7fb901023fdbf920
Devadharshini S <niladeva631@gmail.com>
2026-07-19 14:44:26 +0530

    Create Type casting.java

---
+++ Type casting.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        int a=15;
+        double b=3.7777d;
+        System.out.println("int="+a);
+        System.out.println("d="+b);
+    }
+}
