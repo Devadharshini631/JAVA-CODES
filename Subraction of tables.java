246fd36f21d5a61208f5728a39d04f01e89fe0a8
Devadharshini S <niladeva631@gmail.com>
2026-07-20 14:57:48 +0530

    Create Subraction of tables.java

---
+++ Subraction of tables.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int  n=100;
+        for(int i=1;i<1999;i++)
+        {
+            System.out.println(i+"-"+n+"="+(n-i));
+        }
+    }
+        }
