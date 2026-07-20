e4f9b9eab92f23ec1d43e849c8a4d0bfe748afc8
Devadharshini S <niladeva631@gmail.com>
2026-07-20 14:59:03 +0530

    Create Division of tables.java

---
+++ Division of tables.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int  n=1888;
+        for(int i=1;i<55;i++)
+        {
+            System.out.println(i+"/"+n+"="+(n/i));
+        }
+    }
+        }
