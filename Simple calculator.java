a418d06bd517a60110abdda5db1f7e70a582b457
Devadharshini S <niladeva631@gmail.com>
2026-07-20 15:31:43 +0530

    Create Simple calculator.java

---
+++ Simple calculator.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number;");
+        int a=sc.nextInt();
+        System.out.print("Enter the second number;");
+        int b=sc.nextInt();
+        {
+        System.out.println("Addition="+(a+b));
+        System.out.println("Subraction="+(a-b));
+        System.out.println("Multiplication="+(a*b));
+        System.out.println("Divide="+(a/b));
+        
+        }
+    }
+}
