5627f474744e5bc408ecdbfcc625d4f87111d699
Devadharshini S <niladeva631@gmail.com>
2026-07-21 14:36:31 +0530

    Create Array Traversal.java

---
+++ Array Traversal.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int[] marks={10,20,30,40,50};
+        String[] Subject={"Tamil","English","Maths","Science","SS"};
+        for(int i=0;i<marks.length;i++)
+        {
+            System.out.println(Subject[i] + "=" + marks[i]);
+        }
+    }
+} 
