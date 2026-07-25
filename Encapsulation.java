6039c03390647830c700311205c7424cc09861d5
Devadharshini S <niladeva631@gmail.com>
2026-07-25 14:46:09 +0530

    Create Encapsulation.java

---
+++ Encapsulation.java
@@ -0,0 +1,29 @@
+class student{
+    private int id;
+    private String name;
+    public void setid(int id){
+        this.id=id;
+    }
+        
+        public void setname (String name) {
+            this.name=name;
+        }
+        public int getid(){
+            return id;
+        }
+        public String getname() {
+            return name;
+        }
+    }
+        public class Main{
+            public static void main(String[] args){
+                student s=new student();
+                s.setid(101);
+                s.setname("nila");
+                System.out.println("id:"+s.getid());
+                System.out.println("name:"+s.getname());
+                
+            }
+        }
+        
+    
