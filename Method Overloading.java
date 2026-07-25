486b1b9c098c2b12f7899fcb197f24f0b2220d96
Devadharshini S <niladeva631@gmail.com>
2026-07-25 16:49:19 +0530

    Create Method Overloading.java

---
+++ Method Overloading.java
@@ -0,0 +1,14 @@
+class calculator{
+    int add(int a,int b){
+        return a+b;
+}
+double add(double a,double b){
+    return a+b;
+}
+
+    public static void main(String[] args){
+        calculator c=new calculator();
+        System.out.println(c.add(5,10));
+        System.out.println(c.add(5.5,6.7));
+    }
+}
